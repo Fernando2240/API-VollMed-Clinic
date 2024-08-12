85c176d97b5db0a0af463b583369aa57869da118
Fernando2240 <yfmayy@gmail.com>
2024-08-12 15:21:36 -0500

    add new metods for validations

--- api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,8 +3,13 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
 
+    Boolean existsByPacienteIdAndDateBetween(Long idPaciente, LocalDateTime primerHorario, LocalDateTime ultimoHorario);
+
+    Boolean existsByMedicoIdAndDate(LocalDateTime fecha, LocalDateTime fecha1);
 }
